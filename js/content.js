251101b967bbe5ae93f7f09da18fe5fef2851a1a
unknown <kuangwanjing@22BB97D7264C7C3.internal.baidu.com>
2014-07-25 15:50:08 +0800

    修改插件的样式

--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
 var config = {
 	"name": "content",
-	"urlTemplate": "<div class='url_wrapper'><ul class='url_list'><%for(var i=0;i<this.urls.length;i++){%><li class='info-block'><span class='link-block' title=<%=this.urls[i].link%>><%=this.urls[i].title%></span><span class='del-url-btn'>删除</span></li><%}%></ul><div class='btn'><span class='save'>保存</span><span class='cancel'>取消</span></div></div>",
-	"restoreListTemplate": "<div class='restore_wrapper'><ul class='restore-list'><%for(var i=0;i<this.restore.length;i++){%><li class='restore-item'><div class='item-time' data-status=0><%=i+1%>.创建时间：<%=this.restore[i].time%></div><ul class='item-urls'><%for(var j=0;j<this.restore[i].urls.length;j++){%><li class='item-url' title=<%=this.restore[i].urls[j].link%>><%=this.restore[i].urls[j].title%></li><%}%></ul></li><%}%></ul><div class='btn'><span class='cancel'>取消</span></div>",
+	"urlTemplate": "<div class='url_wrapper'><ul class='url_list'><%for(var i=0;i<this.urls.length;i++){%><li class='info-block'><span class='link-block' title=<%=this.urls[i].link%>><%=this.urls[i].title%></span><span class='del-url-btn'>删除</span></li><%}%></ul><div class='kwj-btn'><span class='save'>保存</span><span class='cancel'>取消</span></div></div>",
+	"restoreListTemplate": "<div class='restore_wrapper'><ul class='restore-list'><%for(var i=0;i<this.restore.length;i++){%><li class='restore-item' items-id=<%=this.restore[i].key%>><div class='item-time' data-status=0><%=i+1%>.创建时间：<%=this.restore[i].time%></div><ul class='item-urls'><%for(var j=0;j<this.restore[i].urls.length;j++){%><li class='item-url' title=<%=this.restore[i].urls[j].link%>><%=this.restore[i].urls[j].title%></li><%}%></ul></li><%}%></ul><div class='kwj-btn'><span class='cancel'>取消</span></div>",
 	"urlListId": "kwj-url-list",
 	"restoreListId": "kwj-restore-list"
 };
